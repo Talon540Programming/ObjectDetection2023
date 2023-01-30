33d63d16d517e79d48c9142e9d2c82f01f35b497
Ayush Pal <ayush_p@icloud.com>
2023-01-29 22:00:21 -0500

    made changes

--- tpu_detection/edgetpumodel.py
+++ tpu_detection/edgetpumodel.py
@@ -158,9 +158,9 @@ class EdgeTPUModel:
         self.interpreter.invoke()
         
         # Scale output
-        output = common.output_tensor(self.interpreter, 0).astype('float32')
+        output = common.output_tensor(self.interpreter, 0).astype('float32') - self.output_zero
         print(output)
-        result = (output - self.output_zero) * self.output_scale
+        result = (output) * self.output_scale
         self.inference_time = time.time() - tstart
         
         if with_nms:
